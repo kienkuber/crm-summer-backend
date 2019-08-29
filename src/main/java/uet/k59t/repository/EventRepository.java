4af1bc192a1de18b2e2910eb36cebe5277defccc
Kien Nguyen Huu <kien.141315@gmail.com>
2019-08-30 00:00:31 +0700

    Update EventRepository.java

--- src/main/java/uet/k59t/repository/EventRepository.java
+++ src/main/java/uet/k59t/repository/EventRepository.java
@@ -3,7 +3,8 @@ package uet.k59t.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uet.k59t.model.Event;
+import org.springframework.data.domain.Pageable;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-    Page<Event> findAllByAccountId(Long accountId);
+    Page<Event> findAllByAccountId(Long accountId, Pageable pageable);
 }
