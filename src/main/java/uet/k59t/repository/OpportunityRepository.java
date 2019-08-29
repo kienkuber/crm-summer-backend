92a566ec7fb2b3b73598768e77b7bb51942ea988
Kien Nguyen Huu <kien.141315@gmail.com>
2019-08-29 23:56:04 +0700

    Update OpportunityRepository.java

--- src/main/java/uet/k59t/repository/OpportunityRepository.java
+++ src/main/java/uet/k59t/repository/OpportunityRepository.java
@@ -3,10 +3,12 @@ package uet.k59t.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uet.k59t.model.Opportunity;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.Optional;
 
 public interface OpportunityRepository extends JpaRepository<Opportunity, Long> {
-    Page<Opportunity> findAllByDeletedIsFalseAndAccountId(Long accountId);
+    Page<Opportunity> findAllByDeletedIsFalseAndAccountId(Long accountId, Pageable pageable);
     Optional<Opportunity> findByDeletedIsFalseAndId(Long id);
 }
